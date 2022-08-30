6930444c8f622cf696858b728af34e9de939fc30
JavyDigade <digade@hotmail.com.ar>
2022-08-29 23:46:31 -0300

    classfuncion 4

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
       <header className="App-header">
         
         <img src={logo} className="App-logo" alt="logo" />
-       <ClassComponent titulo="Prop de componente de clase"/>
-       <FunctionalComponent 
+       <ClassComponent titulo="Prop de componente de clase" numero={5}/>
+       {/* <FunctionalComponent 
           titulo="Prop de comp de funcion"
           numero={3}
           booleano={true}
-        />
+        /> */}
       </header>
     </div>
   );
--- src/components/ClassComponent/ClassComponent.js
+++ src/components/ClassComponent/ClassComponent.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
+
 class ClassComponent extends Component {
-    render(){
+    render() {
+        console.log(this.props);
         return (
         <div>
-            <h2>Hola soy un componente de clase</h2>
+            <h2>Hola soy un {this.props.titulo}</h2>
         </div>
-        )
+        );
     }
 }
 
