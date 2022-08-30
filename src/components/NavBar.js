14f380ea6be1235771e4c068109cbe53b67a34e3
JavyDigade <digade@hotmail.com.ar>
2022-08-29 22:43:27 -0300

    react js para mafer

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+// import logo from '../logo.svg';
+
+const NavBar = () => {
+     return (
+            <div>
+            <img src="http://www.mafer.com.ar/images/maferparaweb2.png" alt="Logo Mafer Electronics" style={{
+                paddingRight: '30px',
+                }} />
+            Mafer Electronics</div>
+            );
+
+    
+};
+
+export default NavBar;
